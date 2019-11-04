98ffa69c6df7bfea52cf2935396f31495972c68a
Emily Lewis <emilytessalewis@gmail.com>
2019-11-04 13:33:49 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//Hint: jQuery methods: .submit(), 
+//preventDefault(), toggleClass(), closest()
+//using 'this' and event delegation
+
+function myAlert {
+    alert("Hello!");
+}
+
+//enter items they need to purchase 
+//by entering text and hitting "Return" 
+//or clicking the "Add item" button
+const addItems = function() {
+    $('#js-shopping-list-form').submit(event){
+        event.preventDefault();
+    addItems();
+    }
+}
+
+
+//check and uncheck items on the list by 
+//clicking the "Check" button
+$('.shopping-list').on('click','.shopping-item-toggle',
+function() {
+    let listItem = $(this).closest('li')
+    .find('.shopping-item')
+    listItem.toggleClass('shopping-item__checked')
+  });
+
+
+
+//permanently remove items from the list
+    $('.shopping-list').on('click',
+    '.shopping-item-delete', function(event){
+        $(this).closest('li').remove();
+    });
+})
